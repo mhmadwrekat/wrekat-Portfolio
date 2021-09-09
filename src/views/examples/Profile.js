d87db04b7c55a7fb5bba5ac58ec7141815f9cc3a
mhmad wrekat <mhmmadwreekkat@gmail.com>
2021-09-09 17:31:03 +0300

    add

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -88,7 +88,7 @@ class Profile extends React.Component {
                         <Button
                           className="mr-4"
                           color="info"
-                          href="#pablo"
+                          href="https://github.com/mhmadwrekat"
                           onClick={e => e.preventDefault()}
                           size="sm"
                         >
@@ -97,7 +97,7 @@ class Profile extends React.Component {
                         <Button
                           className="float-right"
                           color="default"
-                          href="#pablo"
+                          href="https://www.linkedin.com/in/mohammad-alwrekat"
                           onClick={e => e.preventDefault()}
                           size="sm"
                         >
@@ -108,7 +108,7 @@ class Profile extends React.Component {
                     <Col className="order-lg-1" lg="4">
                       <div className="card-profile-stats d-flex justify-content-center">
                         <div>
-                          <span className="heading">22</span>
+                          <span className="heading">122</span>
                           <span className="description">Friends</span>
                         </div>
                         <div>
@@ -116,7 +116,7 @@ class Profile extends React.Component {
                           <span className="description">Photos</span>
                         </div>
                         <div>
-                          <span className="heading">89</span>
+                          <span className="heading">189</span>
                           <span className="description">Comments</span>
                         </div>
                       </div>
@@ -124,31 +124,29 @@ class Profile extends React.Component {
                   </Row>
                   <div className="text-center mt-5">
                     <h3>
-                      Jessica Jones{" "}
-                      <span className="font-weight-light">, 27</span>
+                      Mohammad Wrekat{" "}
+                      <span className="font-weight-light"> , 25</span>
                     </h3>
                     <div className="h6 font-weight-300">
                       <i className="ni location_pin mr-2" />
-                      Bucharest, Romania
+                      Jordan , Amman
                     </div>
                     <div className="h6 mt-4">
                       <i className="ni business_briefcase-24 mr-2" />
-                      Solution Manager - Creative Tim Officer
+                      Software Developer - Full Stack Web Development
                     </div>
                     <div>
                       <i className="ni education_hat mr-2" />
-                      University of Computer Science
+                      Amman Arab University - ASAC
                     </div>
                   </div>
                   <div className="mt-5 py-5 border-top text-center">
                     <Row className="justify-content-center">
                       <Col lg="9">
                         <p>
-                          An artist of considerable range, Ryan — the name taken
-                          by Melbourne-raised, Brooklyn-based Nick Murphy —
-                          writes, performs and records all of his own music,
-                          giving it a warm, intimate feel with a solid groove
-                          structure. An artist of considerable range.
+Have a beclore Degree in software engineering , and web development ( FrontEnd and BackEnd )
+, like programming and web development .
+the excites me the most about tech is it always evolve quickly and new technologies are always emerging .
                         </p>
                         <a href="#pablo" onClick={e => e.preventDefault()}>
                           Show more
