73cc8816ee65bfb74216e41857fb4ca44266874e
mhmad wrekat <mhmmadwreekkat@gmail.com>
2021-09-09 22:35:34 +0300

    edit root

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss?v1.1.0";
 
-import Index from "views/Index.js";
+//import Index from "views/Index.js";
 import Landing from "views/examples/Landing.js";
 //import Login from "views/examples/Login.js";
 import Profile from "views/examples/Profile.js";
@@ -32,16 +32,15 @@ import Profile from "views/examples/Profile.js";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact render={props => <Index {...props} />} />
-      <Route
-        path="/profile-page"
-        exact
-        render={props => <Profile {...props} />}
-      />
       <Route
         path="/landing-page"
         exact
         render={props => <Landing {...props} />}
+      />
+      <Route
+        path="/"
+        exact
+        render={props => <Profile {...props} />}
       />
 
       <Redirect to="/" />
@@ -49,3 +48,7 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
+/*
+      <Route path="/profile-page" exact render={props => <Index {...props} />} />
+
+*/
